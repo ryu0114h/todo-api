621c5fbc394d1139dd98e87293e9d3bfdbf29fe6
星野龍 <ryu0114h@gmail.com>
2024-07-23 16:31:16 +0900

    task削除

--- repository/task.go
+++ repository/task.go
@@ -11,9 +11,10 @@ import (
 
 type TaskRepository interface {
 	GetTasks(companyId uint, limit, offset int) ([]*model.Task, error)
-	GetTask(id, companyId uint) (*model.Task, error)
+	GetTask(companyId, id uint) (*model.Task, error)
 	CreateTask(task *model.Task) (*model.Task, error)
 	UpdateTask(id uint, task *model.Task) (*model.Task, error)
+	DeleteTask(companyId, id uint) error
 }
 
 type taskRepository struct {
@@ -34,9 +35,9 @@ func (r *taskRepository) GetTasks(companyId uint, limit, offset int) ([]*model.T
 	return tasks, nil
 }
 
-func (r *taskRepository) GetTask(id, companyId uint) (*model.Task, error) {
+func (r *taskRepository) GetTask(companyId, id uint) (*model.Task, error) {
 	task := &model.Task{}
-	result := r.db.Preload("Assignee").Where("company_id = ?", companyId).Find(task, "id = ?", id)
+	result := r.db.Preload("Assignee").Where("id = ? AND company_id = ?", id, companyId).Find(task)
 	if result.Error != nil {
 		slog.Info(fmt.Sprintf("error GetTask: %v", result.Error))
 		return nil, myErrors.ErrDb
@@ -60,4 +61,15 @@ func (r *taskRepository) UpdateTask(id uint, task *model.Task) (*model.Task, err
 		return nil, myErrors.ErrDb
 	}
 	return task, nil
+}
+
+func (r *taskRepository) DeleteTask(companyId, id uint) error {
+	result := r.db.Where("id = ? AND company_id = ?", id, companyId).Delete(&model.Task{})
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return myErrors.ErrNotFound
+	}
+	return nil
 }
